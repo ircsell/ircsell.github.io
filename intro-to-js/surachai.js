32f1ad585fc78fdacf507cfa59073175360b26c5
surachai <ircsell@hotmail.com>
2022-02-17 13:59:47 +0700

    intro-js

---
+++ intro-to-js/surachai.js
@@ -0,0 +1,12 @@
+function askMyName() {
+   const name = prompt("Please enter your name");// เรียกใช้คำสั่ง prompt เพื่อถามชื่อ https://www.w3schools.com/jsref/met_win_prompt.asp
+
+   if (name != null) {
+      document.getElementById( "my-name"/*"ใส่ id ของ h1 ในนี้"*/).innerHTML = "My name is " + name;
+   }
+}
+
+function helloWorld() {
+const h1message =  document.getElementById( "my-name"/*"ใส่ id ของ h1 ในนี้"*/).innerHTML;// ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
+                  alert( "h1message" + "สู้งาน สู้ชีวิต แต่ไม่คิดสู้เมีย" /*'ใส่คำคมหลังรถสิบล้อ'*/)
+}
